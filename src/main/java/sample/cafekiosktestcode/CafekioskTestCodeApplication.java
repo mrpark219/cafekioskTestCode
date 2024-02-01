622310cb454a61abdd183e9ea0e0c36fb332523b
mrpark219 <mrpark219@gmail.com>
2024-02-01 16:03:55 +0900

    🎉 Practical Testing: 실용적인 테스트 가이드 강의 시작

---
+++ src/main/java/sample/cafekiosktestcode/CafekioskTestCodeApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekiosktestcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskTestCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafekioskTestCodeApplication.class, args);
+	}
+
+}
