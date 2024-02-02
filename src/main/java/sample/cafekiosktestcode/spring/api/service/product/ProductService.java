5eb1f77ae95e7dd7abd1eddd865438c72ef549a1
mrpark219 <mrpark219@gmail.com>
2024-02-03 00:06:50 +0900

    ✨ 키오스크에서 판매할 제품 도메인에 대한 Presentation Layer, Business Layer, Persistence Layer 생성

---
+++ src/main/java/sample/cafekiosktestcode/spring/api/service/product/ProductService.java
@@ -0,0 +1,27 @@
+package sample.cafekiosktestcode.spring.api.service.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosktestcode.spring.api.service.product.response.ProductResponse;
+import sample.cafekiosktestcode.spring.domain.product.Product;
+import sample.cafekiosktestcode.spring.domain.product.ProductRepository;
+import sample.cafekiosktestcode.spring.domain.product.ProductSellingStatus;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class ProductService {
+
+	private final ProductRepository productRepository;
+
+	public List<ProductResponse> getSellingProducts() {
+		List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+
+		return products.stream()
+				.map(ProductResponse::of)
+				.collect(Collectors.toList());
+	}
+
+}
---
+++ src/main/java/sample/cafekiosktestcode/spring/domain/product/Product.java
@@ -0,0 +1,30 @@
+package sample.cafekiosktestcode.spring.domain.product;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosktestcode.spring.domain.BaseEntity;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String productNumber;
+
+	@Enumerated(EnumType.STRING)
+	private ProductType type;
+
+	@Enumerated(EnumType.STRING)
+	private ProductSellingStatus sellingStatus;
+
+	private String name;
+
+	private int price;
+}
