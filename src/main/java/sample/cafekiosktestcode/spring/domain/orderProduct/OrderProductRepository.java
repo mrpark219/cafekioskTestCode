e0dd262090048d0aa64460f0cf596199f9d91e06
mrpark219 <mrpark219@gmail.com>
2024-02-04 11:36:14 +0900

    👔 중복되는 상품 번호 리스트로 주문을 생성할 수 있도록 수정

---
+++ src/main/java/sample/cafekiosktestcode/spring/domain/orderProduct/OrderProductRepository.java
@@ -0,0 +1,11 @@
+package sample.cafekiosktestcode.spring.domain.orderProduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+
+
+
+}
--- src/test/java/sample/cafekiosktestcode/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosktestcode/spring/api/service/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosktestcode.spring.api.service.order;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosktestcode.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosktestcode.spring.api.service.order.Response.OrderResponse;
+import sample.cafekiosktestcode.spring.domain.order.OrderRepository;
+import sample.cafekiosktestcode.spring.domain.orderProduct.OrderProductRepository;
 import sample.cafekiosktestcode.spring.domain.product.Product;
 import sample.cafekiosktestcode.spring.domain.product.ProductRepository;
 import sample.cafekiosktestcode.spring.domain.product.ProductType;
@@ -27,7 +30,20 @@ class OrderServiceTest {
 	private ProductRepository productRepository;
 
 	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
+	private OrderProductRepository orderProductRepository;
+
+	@Autowired
 	private OrderService orderService;
+
+	@AfterEach
+	void tearDown() {
+		orderProductRepository.deleteAllInBatch();
+		productRepository.deleteAllInBatch();
+		orderRepository.deleteAllInBatch();
+	}
 
 	@DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
 	@Test
@@ -61,7 +77,40 @@ class OrderServiceTest {
 						tuple("001", 1000),
 						tuple("002", 3000)
 				);
+	}
 
+	@DisplayName("중복되는 상품 번호 리스트로 주문을 생성할 수 있다.")
+	@Test
+	void createOrderWithDuplicateProduct() {
+
+		// given
+		LocalDateTime registeredDateTime = LocalDateTime.now();
+
+		Product product1 = createProduct(HANDMADE, "001", 1000);
+		Product product2 = createProduct(HANDMADE, "002", 3000);
+		Product product3 = createProduct(HANDMADE, "003", 5000);
+
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		OrderCreateRequest request = OrderCreateRequest.builder()
+				.productNumbers(List.of("001", "001"))
+				.build();
+
+		// when
+		OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+		// then
+		assertThat(orderResponse.getId()).isNotNull();
+		assertThat(orderResponse)
+				.extracting("registeredDateTime", "totalPrice")
+				.contains(registeredDateTime, 2000);
+
+		assertThat(orderResponse.getProducts()).hasSize(2)
+				.extracting("productNumber", "price")
+				.containsExactlyInAnyOrder(
+						tuple("001", 1000),
+						tuple("001", 1000)
+				);
 	}
 
 	private Product createProduct(ProductType type, String productNumber, int price) {
