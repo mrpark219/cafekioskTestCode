7ed45c879d56f84e4fc4f500b5423e4018fce1a1
mrpark219 <mrpark219@gmail.com>
2024-02-05 14:15:40 +0900

    ✅ ProductType.containsStockType 메서드에 대한 테스트를 @ParameterizedTest로 수행할 수 있도록 추가

--- src/test/java/sample/cafekiosktestcode/spring/domain/product/ProductTypeTest.java
+++ src/test/java/sample/cafekiosktestcode/spring/domain/product/ProductTypeTest.java
@@ -2,24 +2,29 @@ package sample.cafekiosktestcode.spring.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTypeTest {
-	
+
 	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
 	@Test
 	void containsStockType() {
-	    
-	    // given
+
+		// given
 		ProductType givenType = ProductType.HANDMADE;
-		
-	    // when
+
+		// when
 		boolean result = ProductType.containsStockType(givenType);
 
 		// then
-	    assertThat(result).isFalse();
+		assertThat(result).isFalse();
 	}
 
 	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
@@ -36,4 +41,35 @@ class ProductTypeTest {
 		assertThat(result).isTrue();
 	}
 
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@CsvSource({"HANDMADE,false", "BOTTLE,true", "BAKERY,true"})
+	@ParameterizedTest
+	void containsStockTypeParameterized1(ProductType productType, boolean expected) {
+
+		//when
+		boolean result = ProductType.containsStockType(productType);
+
+		//then
+		assertThat(result).isEqualTo(expected);
+	}
+
+	private static Stream<Arguments> provideProductTypesForCheckingStockType() {
+		return Stream.of(
+			Arguments.of(ProductType.HANDMADE, false),
+			Arguments.of(ProductType.BOTTLE, true),
+			Arguments.of(ProductType.BAKERY, true)
+		);
+	}
+
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@MethodSource("provideProductTypesForCheckingStockType")
+	@ParameterizedTest
+	void containsStockTypeParameterized2(ProductType productType, boolean expected) {
+
+		// when
+		boolean result = ProductType.containsStockType(productType);
+
+		// then
+		assertThat(result).isEqualTo(expected);
+	}
 }
